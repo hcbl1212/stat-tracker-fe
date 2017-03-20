82e8f7dfec3aced08fd3bbedca22262bab114052
Brian Ludwig <hcbl1212@batman-tool.local>
2017-03-19 20:39:35 -0700

    added a signout test

---
+++ test/e2e/specs/logout.js
@@ -0,0 +1,43 @@
+// For authoring Nightwatch tests, see
+// http://nightwatchjs.org/guide#usage
+
+module.exports = {
+  'default e2e tests': function (browser) {
+    // automatically uses dev Server port from /config.index.js
+    // default: http://localhost:8080
+    // see nightwatch.conf.js
+    const devServer = browser.globals.devServerURL
+
+    browser
+      .url(devServer)
+      .waitForElementVisible('#app', 5000)
+      .assert.elementPresent('.landing')
+      .assert.containsText('h1', 'Welcome to Your Vue.js App');
+
+    browser.element('css selector', '.sign-in', function(result) {
+      if(result.status != -1) { //Element exists, do something
+        browser.click('.sign-in')
+        .waitForElementVisible('body',5000)
+        .assert.elementPresent('.landing')
+        .assert.containsText('h1', 'Sign In')
+        .setValue('#email-address','bertha.lindgren@example.net')
+        .setValue('#password','password');
+        browser.element('css selector','.login-button',function(result1){
+          if(result1.status != -1){
+            browser.click('.login-button')
+            .waitForElementVisible('.dashboard',5000)
+            .assert.containsText('h1','I am the the dashboard');
+            browser.element('css selector','.sign-out', function(result2){
+              if(result2 != -1){
+                browser.click('.sign-out')
+                .waitForElementVisible('.landing',5000)
+                .assert.containsText('h1', 'Welcome to Your Vue.js App') 
+                .end()
+              } 
+            }); 
+          } 
+        })
+      }
+    }); 
+  }
+}
