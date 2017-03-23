78b91f59aec67699f4a6470d9d57c15b46fb6a48
Brian Ludwig <hcbl1212@batman-tool.local>
2017-03-22 20:59:19 -0700

    modularized the input component, and added files for store

--- src/components/SignInForm.vue
+++ src/components/SignInForm.vue
@@ -5,16 +5,10 @@
           
     <form v-on:submit.prevent='login()'>
       <div class='field-wrap'>
-        <label class='email-address'>
-          {{placeHolder.email}}<span class='req'>*</span>
-        </label>
-        <input v-model='data.body.email' id="email-address" type='email' required autocomplete='off' v-on:keyup="clearPlaceHolder('email-address')"/>
+        <textBox :label='emailLabel' :textbox='emailInput' v-model='data.body.email' value=''></textBox>
       </div>
       <div class='field-wrap'>
-        <label class='password'>
-          {{placeHolder.password}}<span class='req'>*</span>
-        </label>
-        <input v-model='data.body.password' id="password" type='password' required autocomplete='off' v-on:keyup="clearPlaceHolder('password')"/>
+        <textBox :label='passwordLabel' :textbox='passwordInput' v-model='data.body.password' value=''></textBox>
       </div> 
       <button class='button button-block login-button'>Log In</button>
     </form>
@@ -22,6 +16,8 @@
 </template>
 
 <script>
+
+  import TextBoxWithLabel from './TextBoxWithLabel.vue'
 
   export default {
     name: 'SignInForm',
@@ -36,11 +32,29 @@
           rememberMe: false
         },
         error: null,
-        placeHolder: {
-          password: 'Password',
-          email: 'Email Address'
+        emailInput: {
+          type: 'email',
+          id: 'email-address',
+          required: true
+        },
+        emailLabel: {
+          placeHolder: 'Email Address',
+          class: 'email-address'
+        },
+        passwordInput: {
+          type: 'password',
+          id: 'password',
+          required: true
+        },
+        passwordLabel: {
+          placeHolder: 'Password',
+          class: 'password',
+          id: 'password'
         }
       }
+    },
+    components: {
+      textBox: TextBoxWithLabel
     },
     mounted () {
      // console.log(this.$auth.redirect())
@@ -96,62 +110,6 @@
     margin: 40px auto;
     border-radius: 4px;
     box-shadow: 0 4px 10px 4px rgba(19, 35, 47, 0.3);
-  }
-
-  label {
-    position: absolute;
-    -webkit-transform: translateY(6px);
-    transform: translateY(6px);
-    left: 13px;
-    color: rgba(255, 255, 255, 0.5);
-    -webkit-transition: all 0.25s ease;
-    transition: all 0.25s ease;
-    -webkit-backface-visibility: hidden;
-    pointer-events: none;
-    font-size: 22px;
-}
-  label .req {
-    margin: 2px;
-    color: #CCFFCC;
-  }
-  label.active {
-    -webkit-transform: translateY(50px);
-    transform: translateY(50px);
-    left: 2px;
-    font-size: 14px;
-  }
-
-  label.active .req {
-    opacity: 0;
-  }
-
-  label.highlight {
-    color: #ffffff;
-  }
-
-  input, textarea {
-    font-size: 22px;
-    display: block;
-    width: 100%;
-    height: 100%;
-    padding: 5px 10px;
-    background: none;
-    background-image: none;
-    border: 1px solid white;
-    color: #ffffff;
-    border-radius: 0;
-    -webkit-transition: border-color .25s ease, box-shadow .25s ease;
-    transition: border-color .25s ease, box-shadow .25s ease;
-  }
-
-  input:focus, textarea:focus {
-    outline: 0;
-    border-color: #CCFFCC;
-  }
-
-  textarea {
-    border: 2px solid #a0b3b0;
-    resize: vertical;
   }
 
   .field-wrap {
--- src/components/TextBoxWithLabel.vue
+++ src/components/TextBoxWithLabel.vue
@@ -1,14 +1,42 @@
 <template>
-  <label :class='label.class' :id='label.id'>
-    {{placeHolder}}<span class='req' v-if="required">*</span>
-  </label>
-  <input v-model='textbox.model' :id="textbox.id" :type='textbox.type' :required="required" autocomplete='off' v-on:keyup="clearPlaceHolder('label.class')"/>
+  <span>
+    <label :class='label.class' :id='label.id'>
+      {{label.placeHolder}}<span class='req' v-if='textbox.required'>*</span>
+    </label>
+
+    <input 
+      v-if="textbox.type === 'email'"
+      ref='input'
+      v-on:input="emitInput($event.target.value)" :value='value'
+      :id='textbox.id' type='email'
+      :required='textbox.required' autocomplete='off'
+      v-on:keyup="clearPlaceHolder(label.class)"
+    />
+
+    <input
+       v-else-if="textbox.type === 'password'"
+      ref='input'
+      v-on:input="emitInput($event.target.value)" :value='value'
+      :id='textbox.id' type='password'
+      :required='textbox.required' autocomplete='off'
+      v-on:keyup="clearPlaceHolder(label.class)"
+    />
+
+    <input
+       v-else 
+      ref='input'
+      v-on:input="emitInput($event.target.value)" :value='value'
+       type='textbox':id='textbox.id'
+      :required='textbox.required' autocomplete='off'
+      v-on:keyup="clearPlaceHolder(label.class)"
+    />
+  </span>
 </template>
 
 <script>
   export default {
     name: 'textBoxWithLabel',
-    props: ['label', 'textbox','required', 'placeholder']
+    props: ['label', 'textbox', 'value'],
     methods: {
       clearPlaceHolder (labelClass) {
         var element = document.getElementsByClassName(labelClass)[0]
@@ -17,11 +45,70 @@
         } else {
           element.style.display = 'none'
         }
+      },
+      emitInput (value) {
+        this.$emit('input', value)
       }
     }
   }
 </script>
 
 <style scoped>
+  label {
+    position: absolute;
+    -webkit-transform: translateY(6px);
+    transform: translateY(6px);
+    left: 13px;
+    color: rgba(255, 255, 255, 0.5);
+    -webkit-transition: all 0.25s ease;
+    transition: all 0.25s ease;
+    -webkit-backface-visibility: hidden;
+    pointer-events: none;
+    font-size: 22px;
+}
+  label .req {
+    margin: 2px;
+    color: #CCFFCC;
+  }
+  label.active {
+    -webkit-transform: translateY(50px);
+    transform: translateY(50px);
+    left: 2px;
+    font-size: 14px;
+  }
+
+  label.active .req {
+    opacity: 0;
+  }
+
+  label.highlight {
+    color: #ffffff;
+  }
+
+  input, textarea {
+    font-size: 22px;
+    display: block;
+    width: 100%;
+    height: 100%;
+    padding: 5px 10px;
+    background: none;
+    background-image: none;
+    border: 1px solid white;
+    color: #ffffff;
+    border-radius: 0;
+    -webkit-transition: border-color .25s ease, box-shadow .25s ease;
+    transition: border-color .25s ease, box-shadow .25s ease;
+  }
+
+  input:focus, textarea:focus {
+    outline: 0;
+    border-color: #CCFFCC;
+  }
+
+  textarea {
+    border: 2px solid #a0b3b0;
+    resize: vertical;
+  }
+
 
 </style>
